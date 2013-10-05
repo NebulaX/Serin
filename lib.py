20c4fa0d90865df8b27a8396a1b52254bd611c4e
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2013-10-05 18:53:20 +0530

    fixed function calss in lib.py

--- lib.py
+++ lib.py
@@ -67,11 +67,11 @@ def createListenerHere(host, port):
 
 # The main action function. Called when event happens.
 def act(serverip, imsender = 0):
-	conSock = lib.createConnTo(serverip, 444) 
+	conSock = createConnTo(serverip, 444) 
 	print 'Connected to Server'
 
 	# Asking for server status
-	lib.sendMsg(conSock, 'status_of_server')
+	sendMsg(conSock, 'status_of_server')
 	print 'Asking status of server'
 	reply = conSock.recv(1024)
 	conSock.close()
@@ -85,7 +85,7 @@ def act(serverip, imsender = 0):
 		print 'You have selected ' + toSendName
 		conSock = lib.createConnTo(server, 444)
 		# Sending Filename to server
-		lib.sendMsg(conSock, toSendName)
+		sendMsg(conSock, toSendName)
 		reply = conSock.recv(1024)
 		if reply == 'ok':
 			print 'Sent filename to server'
@@ -94,7 +94,7 @@ def act(serverip, imsender = 0):
 	if reply:
 		# I m sender and i m broadcasting the file for reception
 		if imsender == 1:
-			filesender = lib.createListenerHere('', 5555)
+			filesender = createListenerHere('', 5555)
 			filesender.listen(1)
 			print 'Listening for file request'
 			while True:
@@ -104,7 +104,7 @@ def act(serverip, imsender = 0):
 				if req == 'gimme':
 					print 'Connection asked for file, sending now.'
 					toSendFile = open(toSend, 'rb')
-					lib.sendFile(conn, toSendFile)
+					sendFile(conn, toSendFile)
 					break
 			printDone()
 		
@@ -114,10 +114,10 @@ def act(serverip, imsender = 0):
 			replies = reply.split(',')
 			fileName = replies[0]
 			sender = replies[1]
-			sockToFS = lib.createConnTo(sender, 5555)
+			sockToFS = createConnTo(sender, 5555)
 			fil = open(fileName, 'wb')
 			print 'Requesting sender for file'
-			lib.sendMsg(sockToFS, 'gimme')
+			sendMsg(sockToFS, 'gimme')
 			response = sockToFS.recv(1024)
 			while response != '':
 				fil.write(response)
